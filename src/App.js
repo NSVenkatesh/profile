84e05fc4c13c7b373b4f97dce95173af0e81ed55
NSVenkatesh <nsvenkatesh853@gmail.com>
2024-08-06 12:49:32 +0530

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate,
@@ -15,17 +15,17 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App box-border">
-      <Router>
+      <Router basename="/">
         <div className="flex flex-col min-h-screen">
           <Navbar />
           <div className="flex-1 flex flex-col sm:flex-row">
             <main className="flex-1 ml-0 sm:ml-64 transition-all duration-300">
               <Routes>
+                <Route path="/" element={<Navigate to="/profile" />} />
                 <Route path="/profile" element={<Home />} />
                 <Route path="/resume" element={<Resume />} />
                 <Route path="/portfolio" element={<Portfolio />} />
                 <Route path="/contact" element={<Contact />} />
-                <Route path="/" element={<Navigate to="/profile" />} />
                 <Route path="*" element={<Navigate to="/profile" />} />
               </Routes>
             </main>
